7624f808e5d8e2c56270c9c1363ad9815a8713bc
mpdunne <mpdunne@live.com>
2017-03-11 13:48:13 +0000

    Allow lowercase nucleotides. wq:

--- alan.sh
+++ alan.sh
@@ -18,10 +18,10 @@ function alandavies {
 function alanrickman {
                 paste <(grep ">" $1 | sed -r "s/$/\t###/g" | column -t -n -s $'\t' | sed -r "s/###//g") <(sed -r "s/^>(.*)$/£££>\1##K-STQNAILMFWVCRCEDGHYP###/g" $1 | tr '\n' ' ' | sed -r "s/£££/\n/g" | sed -r "s/ //g" | grep -v "\*[A-Z]" | grep -vP "^$" |
                 GREP_COLORS='mt=0;0' egrep --color=always "[-]" | \
-                GREP_COLORS='mt=01;31' egrep --color=always "[A]" | \
-                GREP_COLORS='mt=01;32' egrep --color=always "[C]" | \
-                GREP_COLORS='mt=01;35' egrep --color=always "[G]" | \
-                GREP_COLORS='mt=01;34' egrep --color=always "[T]" | \
+                GREP_COLORS='mt=01;31' egrep --color=always "[Aa]" | \
+                GREP_COLORS='mt=01;32' egrep --color=always "[Cc]" | \
+                GREP_COLORS='mt=01;35' egrep --color=always "[Gg]" | \
+                GREP_COLORS='mt=01;34' egrep --color=always "[Tt]" | \
                 sed -r "s/.*###//g") | less -SR
 }
 
