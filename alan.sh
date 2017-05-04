07ba4cc3a495fdc53ba710636063c689f3511fe2
mpdunne <mpdunne@live.com>
2017-05-04 11:18:05 +0100

    Made text colours darker for lighter backgrounds to improve readability.

--- alan.sh
+++ alan.sh
@@ -8,14 +8,14 @@ function alandavies {
 	paste <(grep ">" $1 | sed -r "s/$/\t###/g" | column -t -n -s $'\t' | sed -r "s/###//g") <(sed -r "s/^>(.*)$/£££>\1##K-STQNAILMFWVCRCEDGHYP###/g" $1 | tr '\n' ' ' | sed -r "s/£££/\n/g" | sed -r "s/ //g" | grep -vP "^$" |
 		GREP_COLORS='mt=01;41' egrep --color=always "[K]*" | \
 		GREP_COLORS='mt=0;0' egrep --color=always "[-]" | \
-		GREP_COLORS='mt=01;42' egrep --color=always "[STQN]" | \
+		GREP_COLORS='mt=01;90;42' egrep --color=always "[STQN]" | \
 		GREP_COLORS='mt=01;44' egrep --color=always "[AILMFWVC]" | \
 		GREP_COLORS='mt=01;41' egrep --color=always "[R]" | \
 		GREP_COLORS='mt=01;105' egrep --color=always "[C]" | \
 		GREP_COLORS='mt=01;45' egrep --color=always "[ED]" | \
-		GREP_COLORS='mt=01;103' egrep --color=always "[G]" | \
+		GREP_COLORS='mt=01;90;103' egrep --color=always "[G]" | \
 		GREP_COLORS='mt=01;46' egrep --color=always "[HY]" | \
-		GREP_COLORS='mt=01;43' egrep --color=always "[P]" | \
+		GREP_COLORS='mt=01;90;43' egrep --color=always "[P]" | \
 		sed -r "s/.*###//g") | less -SR
 }
 
